f119ef4b46858a779bb0d65509b9c914a2713e66
victor salazar <victor.salazar@trescloud.com>
2018-01-23 11:49:09 -0500

    refactorizando codigo corrigiendo help

--- sale_stock_picking_return_invoicing/models/sale_order.py
+++ sale_stock_picking_return_invoicing/models/sale_order.py
@@ -266,17 +266,17 @@ class SaleOrderLine(models.Model):
     
     #columns
     qty_delivered = fields.Float(help='Cantidad total entregada al cliente, se obtiene en base a la cantidad pedida ya gestionada desde bodega.')
-    qty_to_invoice = fields.Float(help='Cantidad pendiente de facturar, se calcula con la resta entre la cantidad entregada, cantidad devuelta'
-                                       ', facturas y notas de crédito emitidas.')
+    qty_to_invoice = fields.Float(help='Cantidad pendiente de facturar, se calcula en base a la siguente fórmula:'
+                                      '(cantidad entregada - cantidad devuelta) - (cantidad facturada - notas de crédito emitidas)')
     qty_to_deliver = fields.Float(compute='_compute_qty_to_deliver', copy=False, store=True,
                                   digits=dp.get_precision('Product Unit of Measure'),
                                   string="Qty to deliver",
-                                  help='Cantidad pendiente de despachar, se calcula en base a los movimientos de ingreso y salida de bodega '
-                                       'pendientes de realizar.')
+                                  help='Cantidad pendiente de despachar, se calcula en base a los movimientos de devolución de mercaderia'
+                                       ' y salida de bodega pendientes de realizar.')
     qty_to_refund = fields.Float(compute='_get_to_invoice_qty', string='Qty to Refund', copy=False, default=0.0,
                                  digits=dp.get_precision('Product Unit of Measure'),
-                                 help='Cantidad pendiente a reembolsar, se calcula con la resta entre '
-                                      'las cantidades facturadas, cantidad reembolsada y la cantidad entregada con sus devoluciones.'
+                                 help='Cantidad pendiente a reembolsar, Se calcula cuando la siguente fórmula retorna un valor negativo:'
+                                     '(cantidad entregada - cantidad devuelta) - (cantidad facturada - notas de crédito emitidas)'
                                       'En base a Cant. a reembolsar se genera la nota de crédito.')
     qty_refunded = fields.Float(compute='_compute_qty_refunded', string='Refunded Qty', copy=False, default=0.0,
                                 digits=dp.get_precision('Product Unit of Measure'),
