23e87c654b2ad7401d1d516c837e3f74a0cf9f92
Andres Calle <andres.calle@trescloud.com>
2018-06-13 12:31:21 -0500

    2018-06-AC-MA-1010

--- purchase_stock_picking_return_invoicing/models/account_invoice.py
+++ purchase_stock_picking_return_invoicing/models/account_invoice.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services
-#           <contact@eficent.com>
+# Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import logging
 
@@ -9,28 +9,32 @@ _logger = logging.getLogger(__name__)
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     def _prepare_invoice_line_from_po_line(self, line):
         '''
         modificamos la cantidad y cuenta contables de las lineas de la factura antes de ser creadas.
         para que las lineas de nota de credito tengan la cantidad del campo a reembolsar.
         '''
-        data = super(AccountInvoice, self)._prepare_invoice_line_from_po_line(line)
+        data = super(AccountInvoice,
+                     self)._prepare_invoice_line_from_po_line(line)
         if line.product_id.purchase_method == 'receive':
+            #TRESCLOUD: Esta formula aparentemente mejorada no funciona en nuestro caso
+            # This formula proceeds from the simplification of full expression:
+            # qty_received + qty_returned - (qty_invoiced + qty_refunded) -
+            # (qty_returned - qty_refunded)
+            # qty = line.qty_received - line.qty_invoiced
             qty = (line.qty_received - line.qty_returned) - (line.qty_invoiced - line.qty_refunded)
             data['quantity'] = qty
         if self.type == 'in_refund':
             invoice_line = self.env['account.invoice.line']
             data['quantity'] *= -1.0
-            data['account_id'] = invoice_line.with_context({
-                                                'journal_id': self.journal_id.id,
-                                                'type': 'in_invoice'
-                                            })._default_account()
-            account = invoice_line.get_invoice_line_account('in_invoice',
-                                                            line.product_id,
-                                                            self.purchase_id.fiscal_position_id,
-                                                            self.env.user.company_id)
+            data['account_id'] = invoice_line.with_context(
+                {'journal_id': self.journal_id.id,
+                 'type': 'in_invoice'})._default_account()
+            account = invoice_line.get_invoice_line_account(
+                'in_invoice', line.product_id,
+                self.purchase_id.fiscal_position_id, self.env.user.company_id)
             if account:
                 data['account_id'] = account.id
         return data
