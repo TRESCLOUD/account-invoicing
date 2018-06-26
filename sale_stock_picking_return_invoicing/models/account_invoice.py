ec5bcf25e13dd41261b9a00c2efbed6086233ec8
Andres Calle <andres.calle@trescloud.com>
2018-06-25 19:34:02 -0500

    Agregada nota de credito con el mismo costo que el reingreso de stock

--- sale_stock_picking_return_invoicing/models/account_invoice.py
+++ sale_stock_picking_return_invoicing/models/account_invoice.py
@@ -12,6 +12,22 @@ import re
 class AccountInvoiceLine(models.Model):    
     _inherit = 'account.invoice.line'
     
+    #HOOK AGREGADO POR TRESCLOUD
+    def _get_anglo_saxon_price_unit_helper(self):
+        '''
+        Redefinimos el precio con el precio del PRIMER reingreso al sistema de stock
+        (de forma nativa tomaba el precio promedio vigente)
+        #TODO: Al implementar notas de credito sobre multiples devoluciones debería
+        rehacerse la lógica para que genere un asiento en la cuenta puente por cada devolución.
+        '''
+        price = self.product_id.standard_price
+        if self.invoice_id.type == 'out_refund' and self.refund_stock_move_ids:
+            #NOTA: De momento funciona pues solo hay un refund_stock_move_ids
+            price = self.refund_stock_move_ids[0].price_unit
+        else:
+            price = super(AccountInvoiceLine, self)._get_anglo_saxon_price_unit_helper()
+        return price
+
     #Columns
     refund_stock_move_ids = fields.Many2many(
         comodel_name='stock.move', 
